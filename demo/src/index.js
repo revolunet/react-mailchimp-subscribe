0f3d22e2ce97be8713071e714c2b88ca70e6ab63
Julien Bouquillon <julien@revolunet.com>
2017-08-30 00:27:16 +0200

    demo fix

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import GithubCorner from 'react-github-corner';
 
 import SubscribeForm from '../../src'
 
 class Demo extends Component {
   render() {
-    const action = "//path/to/mailchimp/url"
-    return <div>
-      <h1>react-mailchimp-subscribe Demo</h1>
-      <SubscribeForm action={action}/>
-    </div>
+    const action = "https://prizoners.us9.list-manage.com/subscribe/post?u=d66d8c5d1ef09114cf8c27ccb&id=3c7edc9b14"
+    return (
+      <div>
+        <h1>react-mailchimp-subscribe Demo</h1>
+        <GithubCorner href="https://github.com/revolunet/react-mailchimp-subscribe" />
+        <SubscribeForm action={action}/>
+      </div>
+    )
   }
 }
 
